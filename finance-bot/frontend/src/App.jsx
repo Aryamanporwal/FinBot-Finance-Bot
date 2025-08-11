ed1d6f6df45e72135f934f64d37445dd7775c3fd
UpendraChaturvedi <upendrachaturvedi099@gmail.com>
2025-08-11 20:47:23 +0530

    preference

--- finance-bot/frontend/src/App.jsx
+++ finance-bot/frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import Auth from "./pages/Auth";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home"
+import NamePrompt from "./components/NamePromt";
+import TopicSelection from "./components/TopicSelection";
 
 const App = () => {
   return (
     <>
-    
-    <Routes>
+      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/auth" element={<Auth />} />
+      <Route path="/name" element={<NamePrompt/>}/>
+      <Route path="/preference" element={<TopicSelection/>}/>
     </Routes>
-   
+      
+      
     </>
   );
 };
